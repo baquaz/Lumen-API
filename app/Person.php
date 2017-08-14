f2f75fbd623b42acd03ba4b50a8aadb065f5ad66
Piotr Błachewicz <blaszku@gmail.com>
2017-08-14 22:25:13 +0200

    Create Person model

---
+++ app/Person.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App;
+
+use Illuminate\Auth\Authenticatable;
+use Laravel\Lumen\Auth\Authorizable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+
+class Person extends Model implements AuthenticatableContract, AuthorizableContract
+{
+    use Authenticatable, Authorizable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'person_id', 'person_name', 'email', 'phone',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at', 'updated_at',
+    ];
+
+    public function item()
+    {
+        return $this->hasOne('Item', 'item_id');
+    }
+}
